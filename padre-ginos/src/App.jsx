b287fb7715411adce4b36b63a46bb534020f8ea2
BasilLangevin <basil.langevin@gmail.com>
2025-05-05 10:37:47 -0700

    feat: add images to pizzas

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -6,9 +6,21 @@ const App = () => {
     return (
         <div>
             <h1>Padre Gino's</h1>
-            <Pizza name="The Pepperoni Pizza" description="Mozzarella Cheese, Pepperoni" />
-            <Pizza name="The Hawaiian Pizza" description="Sliced Ham, Pineapple, Mozzarella Cheese" />
-            <Pizza name="The Big Meat Pizza" description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage" />
+            <Pizza
+                name="The Pepperoni Pizza"
+                description="Mozzarella Cheese, Pepperoni"
+                image="/public/pizzas/pepperoni.webp"
+            />
+            <Pizza
+                name="The Hawaiian Pizza"
+                description="Sliced Ham, Pineapple, Mozzarella Cheese"
+                image="/public/pizzas/hawaiian.webp"
+            />
+            <Pizza
+                name="The Big Meat Pizza"
+                description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+                image="/public/pizzas/big_meat.webp"
+            />
         </div>
     );
 };
