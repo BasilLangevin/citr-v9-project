23abf4581f13b12e646b401bafa31b65561cc59e
BasilLangevin <basil.langevin@gmail.com>
2025-05-05 10:35:11 -0700

    chore: update prettier

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -3,18 +3,18 @@ import { createRoot } from "react-dom/client";
 import Pizza from "./Pizza";
 
 const App = () => {
-  return (
-    <div>
-      <h1>Padre Gino's</h1>
-      <Pizza name="The Pepperoni Pizza" description="Mozzarella Cheese, Pepperoni" />
-      <Pizza name="The Hawaiian Pizza" description="Sliced Ham, Pineapple, Mozzarella Cheese" />
-      <Pizza name="The Big Meat Pizza" description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage" />
-    </div>
-  );
+    return (
+        <div>
+            <h1>Padre Gino's</h1>
+            <Pizza name="The Pepperoni Pizza" description="Mozzarella Cheese, Pepperoni" />
+            <Pizza name="The Hawaiian Pizza" description="Sliced Ham, Pineapple, Mozzarella Cheese" />
+            <Pizza name="The Big Meat Pizza" description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage" />
+        </div>
+    );
 };
 
 scan({
-  enabled: true,
+    enabled: true,
 });
 
 const container = document.getElementById("root");
